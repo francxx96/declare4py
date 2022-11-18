b0d3a308b7ffe5396c2b402aaf8a71b4cf25562b
Martina <ninalandos99@gmail.com>
2022-11-18 12:33:40 +0100

    inizialized methods for class BasicConformanceCheckingResults

--- src/declare4py/models/conformance_checking_results.py
+++ src/declare4py/models/conformance_checking_results.py
@@ -13,4 +13,67 @@ Attributes
 class BasicConformanceCheckingResults:
 
     def __init__(self, dict_results: dict):
-        self.basic_query_checking_results: dict = dict_results
+        self.basic_conformance_checking_results: dict = dict_results
+
+    def pendings(trace_id: int, constr_id: int) -> int:
+        if trace_id == None and constr_id == None:
+            print("ERROR: at least one parameter is expected.")
+        elif trace_id == None:
+            # TODO: returns a dictionary with all the traces in model respecting the specified constraint
+            # TODO: count the elements in dictionary and return that number
+            pass
+        elif constr_id == None:
+            # TODO: returns a dictionary with all the constraints in model for the specified trace
+            # TODO: count the elements in dictionary and return that number
+            pass
+
+    def activations(trace_id: int, constr_id: int) -> int:
+        if trace_id == None and constr_id == None:
+            print("ERROR: at least one parameter is expected.")
+        elif trace_id == None:
+            # TODO: returns a dictionary with all the traces in model respecting the specified constraint
+            # TODO: count the elements in dictionary and return that number
+            pass
+        elif constr_id == None:
+            # TODO: returns a dictionary with all the constraints in model for the specified trace
+            # TODO: count the elements in dictionary and return that number
+            pass
+
+    def violations(trace_id: int, constr_id: int) -> int:
+        if trace_id == None and constr_id == None:
+            print("ERROR: at least one parameter is expected.")
+        elif trace_id == None:
+            # TODO: returns a dictionary with all the traces in model respecting the specified constraint
+            # TODO: count the elements in dictionary and return that number
+            pass
+        elif constr_id == None:
+            # TODO: returns a dictionary with all the constraints in model for the specified trace
+            # TODO: count the elements in dictionary and return that number
+            pass
+
+    def fullfillments(trace_id: int, constr_id: int) -> int:
+        if trace_id == None and constr_id == None:
+            print("ERROR: at least one parameter is expected.")
+        elif trace_id == None:
+            # TODO: returns a dictionary with all the traces in model respecting the specified constraint
+            # TODO: count the elements in dictionary and return that number
+            pass
+        elif constr_id == None:
+            # TODO: returns a dictionary with all the constraints in model for the specified trace
+            # TODO: count the elements in dictionary and return that number
+            pass
+
+    def state(trace_id: int, constr_id: int) -> bool:
+        if trace_id == None and constr_id == None:
+            print("ERROR: at least one parameter is expected.")
+        elif trace_id == None:
+            # TODO: returns a dictionary with all the traces in model respecting the specified constraint
+            # TODO: count the elements in dictionary and return that number
+            pass
+        elif constr_id == None:
+            # TODO: returns a dictionary with all the constraints in model for the specified trace
+            # TODO: count the elements in dictionary and return that number
+            pass
+
+    def clean(self):
+        return self.basic_conformance_checking_results
